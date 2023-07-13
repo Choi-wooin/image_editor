a6e2c9df8ed3d48d04e40988ef99377d3011865e
Choi-wooin <choi.woo.in@gmail.com>
2023-07-13 15:55:54 +0900

    준비 작업 완료

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('Home Screen');
+    return Scaffold(
+      body: Text('Home Screen'),
+    );
   }
 }
