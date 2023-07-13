b0d90cbc3266b7d7708743df7f648b1afbda07b3
Choi-wooin <choi.woo.in@gmail.com>
2023-07-13 22:11:00 +0900

    Footer 구현

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,86 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_editor/component/main_app_bar.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../component/footer.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? image;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      // backgroundColor: Colors.transparent,
+      body: Stack(
+        children: [
+          renderBody(),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: MainAppBar(
+              onPickImage: onPickImage,
+              onSaveImage: onSaveImage,
+              onDeleteImage: onDeleteImage,
+            ),
+          ),
+          if (image != null)
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Footer(
+                onEmoticonTap: onEmoticonTap,
+              ),
+            )
+        ],
+      ),
     );
+  }
+
+  Widget renderBody() {
+    if (image != null) {
+      return Positioned.fill(
+        child: InteractiveViewer(
+          child: Image.file(
+            File(image!.path),
+            fit: BoxFit.cover,
+          ),
+        ),
+      );
+    } else {
+      return Center(
+        child: TextButton(
+          style: TextButton.styleFrom(primary: Colors.grey),
+          onPressed: onPickImage,
+          child: Text('이미지 선택하기'),
+        ),
+      );
+    }
+  }
+
+  void onPickImage() async {
+    final image = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+    );
+    setState(() {
+      this.image = image;
+    });
+  }
+
+  void onSaveImage() {}
+
+  void onDeleteImage() {}
+
+  void onEmoticonTap(int index){
   }
 }
